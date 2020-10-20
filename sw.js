72e7a5a183cf16fbd667037a7893154a3dc9489a
xushuai <1553635476@qq.com>
2020-10-20 10:53:05 +0800

    Signed-off-by: xushuai <1553635476@qq.com>

--- sw.js
+++ sw.js
@@ -27,27 +27,23 @@ var cacheList = [
 ]
 
 // 当浏览器解析完 SW 文件时触发 install 事件
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   // install 事件中一般会将 cacheList 中要换存的内容通过 addAll 方法，请求一遍放入 caches 中
   //https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
   e.waitUntil(
-    caches.open(cacheStorageKey).then(function(cache) {
-      console.log(cache)
+    caches.open('v1').then(function (cache) {
       return cache.addAll(cacheList)
     })
   );
 });
 
 // 激活时触发 activate 事件
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   // active 事件中通常做一些过期资源释放的工作，匹配到就从 caches 中删除
-  var cacheDeletePromises = caches.keys().then(cacheNames => {
-    return Promise.all(cacheNames.map(name => {
-      console.log(name)
-      if (name !== cacheStorageKey) {
-        return caches.delete(name);
-      } else {
-        return Promise.resolve();
+  var cacheDeletePromises = caches.keys().then(keyList => {
+    return Promise.all(keyList.map(function (key) {
+      if (cachesToKeep.indexOf(key) === -1) {
+        return caches.delete(key);
       }
     }));
   });
