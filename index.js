afee7f193f01ae05440ea37c202a24f58f35d581
xushuai <1553635476@qq.com>
2020-10-20 10:11:48 +0800

    Signed-off-by: xushuai <1553635476@qq.com>

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var vm = new Vue({
     el: '.layout',
     data: {
-        columns:[
+        columns: [
             {
                 title: '序号',
                 type: 'index',
-                width:100
+                width: 100
             },
             {
                 title: '单位名称',
@@ -33,42 +33,42 @@ var vm = new Vue({
             },
             {
                 title: '操作',
-                width:300,
-                slot:"action",
+                width: 300,
+                slot: "action",
             },
         ],
-        tableData:[],
-        postList:{
-            "page":1
-            ,"pageSize":10
-            ,"title":""    //标题
-            ,"columnTitle":""    //栏目标题
+        tableData: [],
+        postList: {
+            "page": 1
+            , "pageSize": 10
+            , "title": ""    //标题
+            , "columnTitle": ""    //栏目标题
         },
-        page:{
-            total:0,
-            pageSize:10
+        page: {
+            total: 0,
+            pageSize: 10
         },
-        modal:false,
-        modal2:false,
-        modal3:false,
-        formValidate:{},
-        ruleValidate:{
-//                    name: [
-//                        { required: true, message: 'The name cannot be empty', trigger: 'blur' }
-//                    ],
+        modal: false,
+        modal2: false,
+        modal3: false,
+        formValidate: {},
+        ruleValidate: {
+            //                    name: [
+            //                        { required: true, message: 'The name cannot be empty', trigger: 'blur' }
+            //                    ],
         },
         onLine: navigator.onLine,//网络状态 true：在线；false：离线
     },
-    watch:{
+    watch: {
 
     },
-    computed:{
+    computed: {
 
     },
-    components:{
+    components: {
 
     },
-    mounted(){
+    mounted() {
         this.initSW()
         this.init()
         this.addEventListener()
@@ -78,18 +78,18 @@ var vm = new Vue({
         window.removeEventListener('online', this.updateOnlineStatus);
         window.removeEventListener('offline', this.updateOnlineStatus);
     },
-    methods:{
+    methods: {
 
         /**初始化数据
          * method init
          * */
-        init(){
-            post('http://localhost:8080/ycs_back/columnManage/getColumnContentList',this.postList).then((res)=>{
-                if(res.messageCode==10000){
-                    this.tableData=res.result.columnContentList.list;
-                    this.page.total=res.result.columnContentList.total;
-                }
-            })
+        init() {
+            // post('http://localhost:8080/ycs_back/columnManage/getColumnContentList',this.postList).then((res)=>{
+            //     if(res.messageCode==10000){
+            //         this.tableData=res.result.columnContentList.list;
+            //         this.page.total=res.result.columnContentList.total;
+            //     }
+            // })
         },
         initSW() {
             if ('serviceWorker' in navigator) {
@@ -124,15 +124,23 @@ var vm = new Vue({
             } else {
                 console.log('浏览器不支持serviceWorker')
             }
+            fetch('https://fomenyesu.github.io/service-worker-demo/api.json', {
+                method: 'get'
+            }).then(function (response) {
+                console.log(response);
+            }).catch(function (err) {
+                // Error :(
+                console.log(err);
+            });
         },
         initWebSql() {
-            let ycsDb = openDatabase('ycsDb','1.0','ycs db',50 * 1024 * 1024);
+            let ycsDb = openDatabase('ycsDb', '1.0', 'ycs db', 50 * 1024 * 1024);
             console.log(ycsDb)
             ycsDb.transaction(function (tx) {
-                tx.executeSql('SELECT * FROM T_YCS_PROGRAM WHERE program_id = ?',[1], function (tx, results) {
-                    for (let i = 0; i< results.rows.length; i++) {
+                tx.executeSql('SELECT * FROM T_YCS_PROGRAM WHERE program_id = ?', [1], function (tx, results) {
+                    for (let i = 0; i < results.rows.length; i++) {
                         let program = results.rows.item(i);
-                        alert(program.program_id+"--"+program.code+"--"+program.name+"--"+program.company+"--"+program.program_type+"--"+program.apply_date);
+                        alert(program.program_id + "--" + program.code + "--" + program.name + "--" + program.company + "--" + program.program_type + "--" + program.apply_date);
                     }
                 })
             })
@@ -140,14 +148,14 @@ var vm = new Vue({
         /**添加监听事件
          * method addEventListener
          * */
-        addEventListener(){
+        addEventListener() {
             console.log(1)
             //监听网络连接状态-离线转在线
-            window.addEventListener('online',this.updateOnlineStatus)
+            window.addEventListener('online', this.updateOnlineStatus)
             //监听网络连接状态-在线转离线
-            window.addEventListener('offline',this.updateOnlineStatus)
+            window.addEventListener('offline', this.updateOnlineStatus)
             console.log(this.onLine)
-            if(this.onLine) {
+            if (this.onLine) {
 
             } else {
                 this.initWebSql()
@@ -163,34 +171,34 @@ var vm = new Vue({
         /**查询
          * method search
          * */
-        search(){
-            this.postList.page=1;
+        search() {
+            this.postList.page = 1;
             this.init()
         },
-    // <div class="m-btn-item" @click="view(row)">查看</div>
-    // <div class="m-btn-item" @click="update(row)">更新</div>
-    // <div class="m-btn-item" @click="synchronous(row)">同步</div>
-    // <div class="m-btn-item" @click="resultEntry(row)">结果录入</div>
-    // <div class="m-btn-item" @click="regressionTest(row)">回归测试</div>
+        // <div class="m-btn-item" @click="view(row)">查看</div>
+        // <div class="m-btn-item" @click="update(row)">更新</div>
+        // <div class="m-btn-item" @click="synchronous(row)">同步</div>
+        // <div class="m-btn-item" @click="resultEntry(row)">结果录入</div>
+        // <div class="m-btn-item" @click="regressionTest(row)">回归测试</div>
         /**查看
          * method view
          * */
-        view(item){
+        view(item) {
 
         },
         /**更新
          * method update
          * */
-        update(item){
+        update(item) {
             $.ajax({
                 type: "GET",
                 url: "http://localhost:8080/ycs_back/ycsOffline/getYcsResultInputInfo",
                 data: item.id,
                 dataType: "JSON",
-                success: function(result) {
+                success: function (result) {
                     alert(result.messageCode);
-                    if("10000" == result.messageCode) {
-                        var ycsDb = openDatabase('ycsDb','1.0','ycs db',50 * 1024 * 1024);
+                    if ("10000" == result.messageCode) {
+                        var ycsDb = openDatabase('ycsDb', '1.0', 'ycs db', 50 * 1024 * 1024);
                         ycsDb.transaction(function (tx) {
                             //删除表
                             // tx.executeSql('DROP TABLE IF  EXISTS T_YCS_PROJECT');
@@ -198,19 +206,19 @@ var vm = new Vue({
                             tx.executeSql('CREATE TABLE IF NOT EXISTS T_YCS_PROGRAM (program_id unique , code, name, company, program_type, apply_date)');
                             //创建测试用例-功能性表（如不存在）
                             tx.executeSql('CREATE TABLE IF NOT EXISTS T_YCS_CASE_FUNCTIONAL (functional_id unique , case_id, case_item, program_id, ' +
-                              'case_code, case_purpose, case_init, operating_steps, input_elements, hop_result, test_result, result_status, defect_description, test_status)');
+                                'case_code, case_purpose, case_init, operating_steps, input_elements, hop_result, test_result, result_status, defect_description, test_status)');
                             //创建测试用例-性能效率表
                             //创建测试用例-表头表
                             //创建测试用例-结果表
                             //创建其他类测试用例表
                             //插入项目表信息
                             tx.executeSql('INSERT INTO T_YCS_PROGRAM (program_id, code, name, company, program_type, apply_date) values (?,?,?,?,?,?)',
-                              [1,1,"测试项目","测试单位",10002,"2020/10/10"],function(tx, rs) {
-                                  alert("保存成功！");
-                              },
-                              function(tx, error) {
-                                  alert(error.source + "::" + error.message);
-                              });
+                                [1, 1, "测试项目", "测试单位", 10002, "2020/10/10"], function (tx, rs) {
+                                    alert("保存成功！");
+                                },
+                                function (tx, error) {
+                                    alert(error.source + "::" + error.message);
+                                });
                             //插入功能性用例及结果
                             //插入测试用例-性能效率信息
                             //插入测试用例-表头信息
@@ -225,13 +233,13 @@ var vm = new Vue({
         /**同步
          * method synchronous
          * */
-        synchronous(item){
-            var ycsDb = openDatabase('ycsDb','1.0','ycs db',50 * 1024 * 1024);
+        synchronous(item) {
+            var ycsDb = openDatabase('ycsDb', '1.0', 'ycs db', 50 * 1024 * 1024);
             ycsDb.transaction(function (tx) {
-                tx.executeSql('SELECT * FROM T_YCS_PROGRAM WHERE program_id = ?',[1], function (tx, results) {
-                    for (i = 0; i< results.rows.length; i++) {
+                tx.executeSql('SELECT * FROM T_YCS_PROGRAM WHERE program_id = ?', [1], function (tx, results) {
+                    for (i = 0; i < results.rows.length; i++) {
                         var program = results.rows.item(i);
-                        alert(program.program_id+"--"+program.code+"--"+program.name+"--"+program.company+"--"+program.program_type+"--"+program.apply_date);
+                        alert(program.program_id + "--" + program.code + "--" + program.name + "--" + program.company + "--" + program.program_type + "--" + program.apply_date);
                     }
                 })
             })
@@ -239,27 +247,27 @@ var vm = new Vue({
         /**结果录入
          * method resultEntry
          * */
-        resultEntry(item){
+        resultEntry(item) {
 
         },
         /**回归测试
          * method regressionTest
          * */
-        regressionTest(item){
+        regressionTest(item) {
 
         },
         /**页码改变
          * method onChange
          * */
-        onChange(page){
-            this.postList.page=page;
+        onChange(page) {
+            this.postList.page = page;
             this.init()
         },
         /**page-size改变
          * method onPageSizeChange
          * */
-        onPageSizeChange(pageSize){
-            this.postList.pageSize=pageSize;
+        onPageSizeChange(pageSize) {
+            this.postList.pageSize = pageSize;
             this.init()
         }
     }
--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ var cacheList = [
   'https://cdn.bootcss.com/jquery/1.10.2/jquery.min.js',
   './css/common.css',
   './css/fonts/iview.css',
-  './css/fonts/flex-table.css',
+  './css/fonts/flex-table.scss',
   './css/fonts/ionicons.svg',
   './css/fonts/ionicons.ttf',
   './css/fonts/ionicons.woff',
@@ -42,6 +42,7 @@ self.addEventListener('activate', function(e) {
   // active 事件中通常做一些过期资源释放的工作，匹配到就从 caches 中删除
   var cacheDeletePromises = caches.keys().then(cacheNames => {
     return Promise.all(cacheNames.map(name => {
+      console.log(name,cacheStorageKey)
       if (name !== cacheStorageKey) {
         return caches.delete(name);
       } else {
@@ -54,3 +55,43 @@ self.addEventListener('activate', function(e) {
     Promise.all([cacheDeletePromises])
   );
 });
+
+self.addEventListener('fetch', function(e) {
+  console.log("fetch",e.request.url);
+  if(e.request.url === "https://fomenyesu.github.io/service-worker-demo/api.json"){
+    // 在此编写缓存策略
+    e.respondWith(
+      // 可以通过匹配缓存中的资源返回
+      // caches.match(e.request)
+      // // 也可以从远端拉取
+      fetch(e.request.url)
+      // // 也可以自己造
+      // new Response('自己造')
+      // 也可以通过吧 fetch 拿到的响应通过 caches.put 方法放进 caches
+    );
+  }else{
+    // 在此编写缓存策略
+    e.respondWith(
+      // 可以通过匹配缓存中的资源返回
+      // caches.match(e.request)
+      // // 也可以从远端拉取
+      fetch(e.request.url)
+      // // 也可以自己造
+      // new Response('自己造')
+      // 也可以通过吧 fetch 拿到的响应通过 caches.put 方法放进 caches
+    );
+  }
+});
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.getRegistrations().then(registrations => {
+//     for (const registration of registrations) {
+//       registration.unregister()
+//     }
+//   })
+//   caches.keys().then(keyList => {
+//     return Promise.all(keyList.map(key => {
+//       return caches.delete(key)
+//     }))
+//   })
+// }
